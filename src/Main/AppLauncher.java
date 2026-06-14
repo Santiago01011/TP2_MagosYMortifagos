0979763224130e1d12c870d7e9269e0761ee873e
Santiago01011 <santiagozapata2002@gmail.com>
2026-06-14 13:00:38 -0300

    Initial commit: TP2 Magos y Mortifagos

---
+++ src/Main/AppLauncher.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Main;
+
+/**
+ * 
+ */
+public class AppLauncher {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
